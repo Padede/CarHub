f9a7e3c8f676998e67cd3520c53f9918ac6097af
Padede <avyf23@mail.ru>
2024-04-04 20:07:10 +0300

    init: about component

---
+++ components/About.tsx
@@ -0,0 +1,14 @@
+import Link from "next/link";
+function About() {
+    return (
+      
+      <div className="about pt-12 text-gray-700">       
+        <Link href="/info">
+        <img src=".././about-us.jpg" alt="about_us" className="rounded-full max-width"/>
+        </Link>
+        
+      </div>
+    );
+  }
+  
+  export default About;
