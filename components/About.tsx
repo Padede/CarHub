170140a8311756dee4e0ce1ee3644ca961ae9665
Padede <avyf23@mail.ru>
2024-04-04 20:26:11 +0300

    upd: path of images

--- components/About.tsx
+++ components/About.tsx
@@ -4,7 +4,7 @@ function About() {
       
       <div className="about pt-12 text-gray-700">       
         <Link href="/info">
-        <img src=".././about-us.jpg" alt="about_us" className="rounded-full max-width"/>
+        <img src="./about-us.jpg" alt="about_us" className="rounded-full max-width"/>
         </Link>
         
       </div>
